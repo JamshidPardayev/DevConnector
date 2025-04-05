85ada44143419a06a36db7dea19db1e091d557cb
JamshidPardayev <jamshidnt2024@gmail.com>
2025-04-05 22:16:09 +0500

    "profile page yasaldi"

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 function Dashboard(){
     return (
         <div className='px-[35px] font-raleway'>
-            <h1 className='text-[48px] text-[#17a2b8] mt-[25px] font-bold '>Dashboard</h1>
+            <h1 className='text-[50px] text-[#17a2b8] mt-[25px] font-bold '>Dashboard</h1>
             <p className='flex items-center gap-x-[5px] text-[24px] text-[#333333] mt-[10px]'><FaUser />Welcome New User</p>
             <p className='text-[16px] text-[#333333] mt-[16px]'>You have not yet setup a profile, please add some info</p>
             <Link to="/Profile">
